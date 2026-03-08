8dbd2302a8a21cb4c8e6ef75f8b81a4c9709b1ff
Luiz Carlos Marinho <luizcmoficial@gmail.com>
2026-03-08 12:14:38 -0300

    adicionado router / com html de boas vindas

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import userRouter from "./userRouter";
+import path from 'node:path';
 
 const routes = Router();
 
 routes.use("/users", userRouter)
+
+routes.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.sendFile(path.resolve(__dirname, '../views/welcome.html'));
+});
 
 routes.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).send(error.message);
