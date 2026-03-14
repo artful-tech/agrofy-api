842e380ce709dd4e30034136b3b25b42462ef91c
Luiz Carlos Marinho <luizcmoficial@gmail.com>
2026-03-14 13:32:09 -0300

    docs, prototipo front end e grupo de rotas adicionados

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,20 @@ const app = express();
  
 app.use(morgan('tiny'));
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "default-src": ["'self'"],
+        "script-src": ["'self'", "https://cdn.tailwindcss.com", "https://cdn.jsdelivr.net", "'unsafe-inline'"],
+        "style-src": ["'self'", "https://cdn.jsdelivr.net", "'unsafe-inline'"],
+        "connect-src": ["'self'", "https://cdn.jsdelivr.net"],
+        "img-src": ["'self'", "data:", "blob:"],
+      },
+    },
+  })
+);
 app.use(express.json());
 
 // ROTAS DA APLICAÇÃO
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,16 +2,22 @@ import { Router, Request, Response, NextFunction } from 'express';
 import userRouter from "./userRouter";
 import path from 'node:path';
 
-const routes = Router();
+const root = Router();
+const apiRouter = Router();
 
-routes.use("/users", userRouter)
+apiRouter.use("/users", userRouter);
+root.use("/api", apiRouter);
 
-routes.use("/", (req: Request, res: Response, next: NextFunction) => {
-    res.sendFile(path.resolve(__dirname, '../../public/welcome.html'));
+root.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.sendFile(path.resolve(__dirname, '../../public/front-end-example.html'));
 });
 
-routes.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+// ERROS
+root.use((req: Request, res: Response) => {
+    res.status(404).send("<h1>ERRO 404: PÁGINA NÃO EXISTE</h1>");
+});
+root.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).send(error.message);
 });
 
-export default routes;
+export default root;
