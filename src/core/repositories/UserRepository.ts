0d8353671ed5def255a3bae4dc985f075ca0b482
Luiz Carlos <luizcmoficial@gmail.com>
2026-04-24 12:07:04 -0300

    refactor: coisas básicas de formatação em UserRepository

--- src/core/repositories/UserRepository.ts
+++ src/core/repositories/UserRepository.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { IUserRepository } from './interfaces'
 import { UserModel } from "../models/UserModel";
 
@@ -32,6 +32,4 @@ export class UserRepository implements IUserRepository {
     public delete(id: string): Promise<boolean> {
         throw new Error("Method not implemented.");
     }
-
-    
 }
