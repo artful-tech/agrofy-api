7c1b4d750eaa7caf3dd75c8a9149d7761eac956d
Luiz Carlos <luizcmoficial@gmail.com>
2026-04-25 15:33:24 -0300

    fixed: corrigido erro de importacao

--- src/infra/http/auth/AuthLogin.ts
+++ src/infra/http/auth/AuthLogin.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { UserUsecase } from "../../../core/usecases/UserUsecase";
-import { CreateUserDto } from "../../../shared/dtos/UserDto";
+import { UserDtoCreate } from "../../../shared/dtos/UserDto";
 
 export class AuthLogin {
     constructor(private userUsecase: UserUsecase) { }
 
-    public login = async (req: Request<CreateUserDto>, res: Response): Promise<Response> => {
+    public login = async (req: Request<UserDtoCreate>, res: Response): Promise<Response> => {
         
         return res.json();
     }
