8cb39e8c33e496a70ff93c0f627e952d230f9dbd
Luiz Carlos <luizcmoficial@gmail.com>
2026-04-25 17:29:53 -0300

    feat: people dto com validacao por zod adicionado

---
+++ src/shared/dtos/PeopleDto.ts
@@ -0,0 +1,34 @@
+import { $Enums } from "@prisma/client";
+import z from "zod";
+
+
+export const createPeopleSchema = z.object({
+    name: z.string().min(2).max(150),
+    cellphone: z.string().min(11).max(12).optional(),
+    photo: z.string().optional(),
+    userId: z.uuid("ID inválido")
+});
+
+export const updatePeopleSchema = z.object({
+    id: z.uuid("ID inválido"),
+    name: z.string().min(2).max(150).optional(),
+    cellphone: z.string().min(11).max(12).optional().optional(),
+    role: z.enum($Enums.Role).optional(),
+    photo: z.string().optional().optional(),
+    userId: z.uuid().optional()
+});
+
+export type PeopleDtoCreate = z.infer<typeof createPeopleSchema>;
+export type PeopleDtoUpdate = z.infer<typeof updatePeopleSchema>;
+
+export type PeopleDtoView = {
+    name: string;
+    id: string;
+    cellphone: string | null;
+    role: $Enums.Role;
+    photo: string | null;
+    userId: string;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+};
