8f40a7ae0081fe2f39fb65a888f16753026ddd2f
Luiz Carlos <luizcmoficial@gmail.com>
2026-04-25 20:05:00 -0300

    feat: adicionado middleware de validacao no PeopleRouter

--- src/infra/http/routers/PeopleRouter.ts
+++ src/infra/http/routers/PeopleRouter.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { PeopleController } from "../controllers/PeopleController";
+import { ValidationMiddleware } from "../middlewares/ValidationMiddleware";
+import { createPeopleSchema, updatePeopleSchema } from "../../../shared/dtos/PeopleDto";
 
 
 export default class PeopleRouter {
@@ -11,8 +13,16 @@ export default class PeopleRouter {
 
         peopleRouter.get('/', this.peopleController.index);
         peopleRouter.get('/:peopleId', this.peopleController.getOne);
-        peopleRouter.post('/', this.peopleController.create);
-        peopleRouter.patch('/', this.peopleController.update);
+        peopleRouter.post(
+            '/', 
+            ValidationMiddleware.validate(createPeopleSchema),
+            this.peopleController.create
+        );
+        peopleRouter.patch(
+            '/', 
+            ValidationMiddleware.validate(updatePeopleSchema),
+            this.peopleController.update
+        );
         peopleRouter.delete('/', this.peopleController.delete);
 
         return peopleRouter;
