d88acdc1ca6e838ea9ffc9aae3faa2d19681dc92
Luiz Carlos <luizcmoficial@gmail.com>
2026-04-27 00:08:23 -0300

    feat: aprimorado validacao do create para evitar dados duplicados

--- src/core/repositories/CropRepository.ts
+++ src/core/repositories/CropRepository.ts
@@ -1,22 +1,40 @@
 import { PrismaClient } from "@prisma/client";
 import { ICropRepository } from "./interfaces";
 import { CropModelCreate, CropModel, CropModelUpdate } from "../models/CropModel";
+import { AppError } from "../errors/AppError";
 
 
 export class CropRepository implements ICropRepository {
     constructor(private prisma: PrismaClient) {}
 
     public findAll = async (): Promise<CropModel[]> => {
-        return await this.prisma.crop.findMany();
+        return await this.prisma.crop.findMany({
+            where: { deletedAt: null }
+        });
     }
 
     public findOne = async (id: string): Promise<CropModel> => {
         return await this.prisma.crop.findUniqueOrThrow({
-            where: { id: id }
+            where: { 
+                id: id,
+                deletedAt: null
+            }
         });
     }
 
     public create = async (model: CropModelCreate): Promise<string> => {
+        const cropExists = await this.prisma.crop.findFirst({
+            where: {
+                name: model.name,
+                variety: model.variety ?? null,
+                deletedAt: null
+            }
+        });
+
+        if (cropExists) {
+            throw new AppError('Já existe uma planta cadastrada com esse nome e variedade', 409)
+        }
+
         const crop = await this.prisma.crop.create({
             data: model
         });
@@ -25,7 +43,10 @@ export class CropRepository implements ICropRepository {
 
     public update = async (id: string, model: CropModelUpdate): Promise<CropModel> => {
         return await this.prisma.crop.update({
-            where: { id: id },
+            where: { 
+                id: id,
+                deletedAt: null
+            },
             data: model
         });
     }
@@ -34,5 +55,18 @@ export class CropRepository implements ICropRepository {
         await this.prisma.crop.delete({
             where: { id: id }
         });
+
+        const crop = await this.prisma.crop.findUniqueOrThrow({
+            where: { 
+                id: id,
+                deletedAt: null
+             }
+        });
+
+        await this.prisma.crop.update({
+            where: { id: crop.id },
+            data: { deletedAt: new Date() }
+        })
+        
     }
 }
