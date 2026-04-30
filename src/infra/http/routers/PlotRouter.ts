4f0980c58220343bf1b7228d92ceea9f1270691d
Luiz Carlos <luizcmoficial@gmail.com>
2026-04-29 23:13:02 -0300

    feat: todos os métodos HTTP adicionados para Plot

--- src/infra/http/routers/PlotRouter.ts
+++ src/infra/http/routers/PlotRouter.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { PlotController } from "../controllers/PlotController";
 import { RouteDisplay } from "../../utils/RouteDisplay";
+import { ValidationMiddleware } from "../middlewares/ValidationMiddleware";
+import { idUUIDParamSchema } from "../../../shared/dtos";
+import { createPlotSchema, updatePlotSchema } from "../../../shared/dtos/PlotDto";
 
 
 export default  class PlotRouter {
@@ -11,6 +14,30 @@ export default  class PlotRouter {
         const router = Router();
 
         router.get('/', this.plotController.index);
+
+        router.get(
+            '/:id',
+            ValidationMiddleware.validate(idUUIDParamSchema, 'params'),
+            this.plotController.getOne
+        );
+        
+        router.post(
+            '/',
+            ValidationMiddleware.validate(createPlotSchema),
+            this.plotController.create
+        );
+        
+        router.patch(
+            '/',
+            ValidationMiddleware.validate(updatePlotSchema),
+            this.plotController.update
+        );
+        
+        router.delete(
+            '/:id',
+            ValidationMiddleware.validate(idUUIDParamSchema, 'params'),
+            this.plotController.delete
+        );
 
         RouteDisplay.scan(router, "/api/plot");
 
