bff5a34c17ba45f6626bc4ad8011d63b6c8cedcb
Luiz Carlos <luizcmoficial@gmail.com>
2026-04-29 23:44:29 -0300

    feat: adicionado api de clima provisoriamente

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
- 
+
 const PORT = parseInt(`${process.env.PORT || 8080}`);
 const BRIGHT = "\x1b[1m";
 const GREEN = "\x1b[32m";
 const RESET = "\x1b[0m";
- 
-import app from './app';
 
-app.listen(PORT, () => console.log(`\n${BRIGHT}${GREEN}🚀 Agrofy-API is blazing fast at http://localhost:${PORT}${RESET}\n`));
+import app from './app';
+import { WeatherApiService } from './infra/services/WeatherApiService';
+
+app.listen(PORT, () => {
+    console.log(`\n${BRIGHT}${GREEN}🚀 Agrofy-API is blazing fast at http://localhost:${PORT}${RESET}\n`)
+});
